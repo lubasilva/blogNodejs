e985e5858925dc89edb9436a6d48b515560de7f9
Lucas Batista <lubapisilva@gmail.com>
2021-05-27 10:20:20 -0300

    complete artigles, create home

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,11 @@ const Article = require('./Article')
 const slugify = require('slugify')
 
 router.get('/admin/articles', (req, res) => {
-    res.send('Articles route')
+    Article.findAll({
+        include: [{model: Category}]
+    }).then(articles => {
+    res.render('admin/articles/index', {articles: articles})
+    })
 })
 
 router.get('/admin/articles/new', (req, res) => {
@@ -16,18 +20,37 @@ router.get('/admin/articles/new', (req, res) => {
 
 router.post('/articles/save', (req, res) => {
     let title = req.body.title
-    let bodyInfo = req.body.bodyInfo
+    let body = req.body.body
     let category = req.body.category
 
     Article.create({
         title: title,
         slug: slugify(title),
-        bodyInfo: bodyInfo,
+        body: body,
         categoryId: category
 
     }).then(() => {
-        res.redirect('admin/articles')
+        res.redirect('/admin/articles')
     })
+})
+
+router.post('/articles/delete', (req, res) => {
+    let id = req.body.id
+    if(id != undefined){
+        if(!isNaN(id)){
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect('/admin/articles')
+            })
+        }else{
+            res.redirect('/admin/articles')
+        }
+    }else{
+        res.redirect('/admin/articles')
+    }
 })
 
 module.exports = router
